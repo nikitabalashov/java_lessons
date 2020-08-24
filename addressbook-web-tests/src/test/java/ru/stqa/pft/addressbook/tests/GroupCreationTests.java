3583cbc265fdc2caab8f08f96810cd9aa6cd0e03
nikitabalashov <snoopy_forever@rambler.ru>
2020-08-24 22:24:53 +0300

    Итог лекции 2.7

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("testio1", "testio2", "testio3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
+   // app.getGroupHelper().wd.findElement(By.linkText("Logout")).click();
+  }
+
+}
