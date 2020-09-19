ff64699d3fece153174fc993d2872cf34e75827a
nikitabalashov <snoopy_forever@rambler.ru>
2020-09-20 01:41:30 +0300

    Задание 12

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -2,20 +2,30 @@ package ru.stqa.pft.addressbook.model;
 
 import java.util.Objects;
 import java.io.File;
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 
 public class ContactData {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+  @Expose
   private  String firstname;
+  @Expose
   private  String lastname;
   private  String nickname;
   private  String company;
+  @Expose
   private  String address;
+  @Expose
   private  String mobile;
   private  String email;
   private String work;
   private String group;
   private String allPhones;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
   private String allEmails;
   private File photo;
